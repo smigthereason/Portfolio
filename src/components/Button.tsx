6f67c5ea73f666dec907f92aef2a46514086e2aa
Victor <victor.dmaina@gmail.com>
2026-03-12 16:59:36 +0300

    Update Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-import '../styles/Button.css';
+import React from "react";
+import "../styles/Button.css";
 
 const Button: React.FC = () => {
   const handleClick = () => {
     // Open the Google Drive link in a new tab for file download
-    window.open('https://drive.google.com/file/d/1BwzNdgpYjqxiKSInQMglt412stTgS5Lv/view?usp=sharing', '_blank');
+    window.open(
+      "https://drive.google.com/file/d/1qiGV8sN8W-sqccNYlOcGrN8235THOmue/view?usp=sharing",
+      "_blank",
+    );
   };
 
   return (
